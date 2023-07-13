26d7657bb931dc3ffb336536b3aac004febb8f77
unknown <mary.liss133@gmail.com>
2023-07-13 17:13:10 +0500

    fix

--- project/backend/libs/shared/app-types/src/lib/notify-user.interface.ts
+++ project/backend/libs/shared/app-types/src/lib/notify-user.interface.ts
@@ -3,7 +3,7 @@ import { NotifyMessage } from "./notify-message.enum";
 export interface NotifyUser {
   id?: number;
   userId: number;
-  initiatorId?: string;
+  initiatorId?: number;
   initiatorName?: string;
   text: NotifyMessage;
   dateNotify: Date;
